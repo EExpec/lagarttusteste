cb75e0015aad4c8baedf5014dc3917e6263b844c
Cauã Santos <you@example.com>
2025-10-07 07:27:11 -0300

    build ready for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,8 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: "/lagarttusteste/", // necessário para GitHub Pages
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react()], // sem componentTagger
+  base: "/lagarttusteste/", // nome exato do repo
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
